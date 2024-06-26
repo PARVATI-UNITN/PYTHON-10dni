c47c8da0deba3add0222a133041fb8daa21a728b
𝖭𝖸X <nyra.official@icloud.com>
2024-06-26 21:54:43 +0530

    Update 02dni.py

--- 02dni.py
+++ 02dni.py
@@ -1,14 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # Find gmail entered is of legit format (clean email)
-
-# The username shouldn't start or end with '.'
-# The username should be between 6 and 30 character
-# If user name contains more than 8 characters it should contain atleast one alphabet
-
-# In[1]:
-
+'''
+The username shouldn't start or end with '.'
+The username should be between 6 and 30 character
+If user name contains more than 8 characters it should contain atleast one alphabet
+'''
 
 import re
 
@@ -32,4 +27,3 @@ else:
     domain=gmail[index:]
     #domain=domain[:-4]
     print("Not a Gmail ID. Maybe from some other Domain which is here ", domain)
-
