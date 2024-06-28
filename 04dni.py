dbe4e7f0cd49a963ec2e38ab80c158c5c77b96d0
𝖭𝖸X <nyra.official@icloud.com>
2024-06-28 23:54:11 +0530

    Add files via upload

---
+++ 04dni.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Simple program for Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
+
+# In[1]:
+
+
+st=input("INPUT A STRING CONTAINING 'bob' (like-bobis,juhwdhxjbobsiolkwkmbobnjs)\n")
+
+times=st.count("bob")
+if times!=0:
+    print(f"Number of times the string 'bob' occurs is {times} times")
+else:
+    print(f"String 'bob' doesn't occur")
+
