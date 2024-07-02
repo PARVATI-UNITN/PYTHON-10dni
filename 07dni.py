192ee598b0833f1554c08349ac63a37cf87e256c
𝖭𝖸X <nyra.official@icloud.com>
2024-07-02 23:41:55 +0530

    Add files via upload

---
+++ 07dni.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Interview Programs
+'''
+6- Bubble Sort
+'''
+
+
+# In[1]:
+
+
+num = int(input("ENTER THE NUMBER OF ELEMENTS TO BE SORTED: "))
+num_list = []
+
+print("Enter the Elements: ")
+for i in range(num):
+    element = int(input(f"{i + 1} : "))  # Convert input to integer
+    num_list.append(element)
+
+# Sorting using bubble sort algorithm
+for j in range(num):
+    for i in range(num - 1):
+        if num_list[i] > num_list[i + 1]:
+            num_list[i], num_list[i + 1] = num_list[i + 1], num_list[i]
+
+print("Sorted List:", num_list)
+
