774c5a1ea2216abc7b445efb8312f41d9c3f83f7
PARVATI <173805272+PARVATI-UNITN@users.noreply.github.com>
2024-09-12 20:02:47 +0530

    Add files via upload

---
+++ 9dni.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Calculate simple interest.
+
+# In[1]:
+
+
+p=float(input('Enter the PRINCIPLE AMOUNT: '))
+n=float(input('Enter the NUMBER OF YEARS: '))
+r=int(input('Enter the RATE OF INTEREST '))
+si=(p*n*r)/100
+print('Simple Interest is ',si)
+
+
+# In[ ]:
+
+
+
+
