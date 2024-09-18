a747fbc7adbbaf3eca970e7dbbcc06b0063d74ce
PARVATI <173805272+PARVATI-UNITN@users.noreply.github.com>
2024-09-18 19:45:43 +0530

    Update 08dni.py

--- 08dni.py
+++ 08dni.py
@@ -1,12 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # Calculate volume of cube.
-
-# In[1]:
-
 
 side=float(input("Enter the side value of cube: "))
 volume=side**3
 print('Volume of cube is ',volume)
-
